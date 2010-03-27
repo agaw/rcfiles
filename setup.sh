e6e945c7db3b8eeaff5843e7eeb0349c5969f6c0
Andrew Naoum <andrew@naoum.net.au>
2010-03-27 15:22:15 +1100

    Fixed setup script to not link README.macosx

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 mkdir -p ~/backup
-mv -f ~/.Xresources ~/.bash ~/.bash_logout ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.inputrc ~/.pythonrc.py ~/.rtorrent.rc ~/.screenrc ~/.ssh ~/.vim ~/.vimrc ~/.terminfo ~/backup
+mv -f ~/.Xresources ~/.bash ~/.bash_logout ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.inputrc ~/.pythonrc.py ~/.rtorrent.rc ~/.screenrc ~/.ssh ~/.vim ~/.vimrc ~/backup
 
 for f in $(ls -A);
 do
-	if [ "$f" != ".git" -a "$f" != "setup.sh" -a "$f" != "README" -a "$f" != "README.terminfo" ]; then 
+	if [ "$f" != ".git" -a "$f" != "setup.sh" -a "$f" != "README" -a "$f" != "README.macosx" ]; then 
 		ln -s `cd \`dirname $f\`; pwd`/`basename $f` ~/;
 	fi
 done
